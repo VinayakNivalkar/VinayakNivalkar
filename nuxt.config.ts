0f8c5bd28b81f97ff2e4e6fc5f8d5c6e569b060b
Vinayak Nivalkar <57988280+VinayakNivalkar@users.noreply.github.com>
2024-01-17 18:14:03 +0530

    removed assets and related plugins

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,6 @@
-//@ts-ignore
-import vsharp from 'vite-plugin-vsharp';
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  vite: {
-    plugins: [vsharp()],
-  },
   modules: [
     "@nuxtjs/tailwindcss",
     "nuxt-icon",
