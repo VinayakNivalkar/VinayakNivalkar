e399ac64daa6369043b2ff394411674ff80b26ce
Vinayak Nivalkar <57988280+VinayakNivalkar@users.noreply.github.com>
2024-01-17 18:13:50 +0530

    accessibility improvements

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="mt-20">
-    <NuxtImg class="object-fill h-96 w-96" v-bind:src="SelectedPxlArt"/>
+    <NuxtImg class="object-fill h-96 w-96" v-bind:src="SelectedPxlArt" alt=""/>
   </div>
   <div class="mb-5 px-2 py-1 bg-red-50 hover:bg-red-100 cursor-pointer" @click="switchPxlArt">
     <h1 class="font-serif font-extrabold text-3xl text-gray-800">Vinayak Nivalkar</h1>
@@ -14,21 +14,21 @@
     <p class="font-sans font-semibold text-sm">Have an Idea 💭 or Just wanna say Hii 👋</p>
     <p class="font-sans font-semibold text-sm">Here's how you can reach me</p>
     <div class="grid grid-cols-4 my-2">
-      <a href="mailto:hello@vnyk.me" class="transition-all ease-in-out duration-250 delay-100 flex flex-row justify-center items-center bg-gradient-to-r from-indigo-500 to-pink-500 text-white hover:saturate-150 hover:scale-[110%] px-2.5 py-2.5 rounded-lg m-2 col-span-3">
+      <a href="mailto:hello@vnyk.me" aria-label="Primary Email" class="transition-all ease-in-out duration-250 delay-100 flex flex-row justify-center items-center bg-gradient-to-r from-indigo-500 to-pink-500 text-white hover:saturate-150 hover:scale-[110%] px-2.5 py-2.5 rounded-lg m-2 col-span-3">
         <Icon name="mdi:email" class="flex-none size-5" />
         <p class="font-bold font-sans text-xs md:text-sm mx-1">hello@vnyk.me</p>
       </a>
-      <a target="_blank" href="https://in.linkedin.com/in/vinayak-nivalkar-5988a519a" class="transition-all ease-in-out duration-250 delay-100 flex flex-row justify-center items-center bg-gradient-to-r from-blue-500 to-blue-700 text-white hover:saturate-150 hover:scale-[110%] px-2.5 py-2.5 rounded-lg m-2 col-span-1">
+      <a target="_blank" aria-label="LinkedIn Link" href="https://in.linkedin.com/in/vinayak-nivalkar-5988a519a" class="transition-all ease-in-out duration-250 delay-100 flex flex-row justify-center items-center bg-gradient-to-r from-blue-500 to-blue-700 text-white hover:saturate-150 hover:scale-[110%] px-2.5 py-2.5 rounded-lg m-2 col-span-1">
         <Icon name="uil:linkedin" class="flex-none size-5" />
       </a>
-      <a href="mailto:vinayaknivalkar2002@gmail.com" class="transition-all ease-in-out duration-250 delay-100 flex flex-row justify-center items-center bg-gradient-to-r from-green-500 to-blue-500 text-white hover:saturate-150 hover:scale-[110%] px-2.5 py-2.5 rounded-lg m-2 col-span-4">
+      <a href="mailto:vinayaknivalkar2002@gmail.com" aria-label="Secondary Email" class="transition-all ease-in-out duration-250 delay-100 flex flex-row justify-center items-center bg-gradient-to-r from-green-500 to-blue-500 text-white hover:saturate-150 hover:scale-[110%] px-2.5 py-2.5 rounded-lg m-2 col-span-4">
         <Icon name="mdi:email" class="flex-none size-5" />
         <p class="font-bold font-sans text-xs md:text-sm mx-1">vinayaknivalkar2002@gmail.com</p>
       </a>
-      <a target="_blank" href="https://github.com/VinayakNivalkar" class="transition-all ease-in-out duration-250 delay-100 flex flex-row justify-center items-center bg-gradient-to-r from-black to-gray-600 text-white hover:saturate-150 hover:scale-[110%] px-2.5 py-2.5 rounded-lg m-2 col-span-1">
+      <a target="_blank" aria-label="Github Link" href="https://github.com/VinayakNivalkar" class="transition-all ease-in-out duration-250 delay-100 flex flex-row justify-center items-center bg-gradient-to-r from-black to-gray-600 text-white hover:saturate-150 hover:scale-[110%] px-2.5 py-2.5 rounded-lg m-2 col-span-1">
         <Icon name="uil:github" class="flex-none size-5" />
       </a>
-      <a href="tel:+918433527267" class="transition-all ease-in-out duration-250 delay-100 flex flex-row justify-center items-center bg-gradient-to-r from-red-500 to-pink-500 text-white hover:saturate-150 hover:scale-[110%] px-2.5 py-2.5 rounded-lg m-2 col-span-3">
+      <a href="tel:+918433527267" aria-label="Phone Number" class="transition-all ease-in-out duration-250 delay-100 flex flex-row justify-center items-center bg-gradient-to-r from-red-500 to-pink-500 text-white hover:saturate-150 hover:scale-[110%] px-2.5 py-2.5 rounded-lg m-2 col-span-3">
         <Icon name="mdi:phone" class="flex-none size-5" />
         <p class="font-bold font-sans text-xs md:text-sm mx-1">+91 8433527267</p>
       </a>
