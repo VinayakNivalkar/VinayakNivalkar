1b564c3d97bdd101bef7c046b845edf3931556ff
Vinayak Nivalkar <57988280+VinayakNivalkar@users.noreply.github.com>
2024-01-20 23:44:51 +0530

    blog page and related additions

--- app.vue
+++ app.vue
@@ -1,10 +1,21 @@
 <template>
   <div class="relative isolate overflow-x-hidden w-screen h-screen flex md:flex-row flex-col">
-    <div class="transition-all ease-in-out delay-100 duration-300 absolute flex bg-[url('/jpg/me_at_beach_mini.jpeg')] md:bg-none bg-center w-full h-full md:blur-none blur-sm bg-cover md:opacity-100 opacity-10 md:w-1/2">
+    <div class="transition-all ease-in-out delay-100 duration-300 absolute flex bg-[url('/jpg/me_at_beach_mini.jpeg')] md:bg-none bg-center w-full h-full md:blur-none blur-sm bg-cover md:opacity-100 opacity-10" :class="SelectedPage === '/' ? 'md:w-1/2' : 'md:w-1/4'">
       <NuxtImg alt="" height="1440px" quality="70" src="/jpg/me_at_beach.jpg" format="webp" class="flex md:self-auto self-center object-cover w-full invisible md:visible"/>
     </div>
-    <div class="transition-all ease-in-out delay-100 duration-300 absolute bg-transparent flex flex-col items-center justify-center w-screen h-screen overflow-y-scroll md:right-0 pt-60 md:w-1/2">
+    <div class="transition-all ease-in-out delay-100 duration-300 absolute bg-transparent flex flex-col items-center justify-center w-screen h-screen overflow-y-scroll md:right-0" :class="SelectedPage === '/' ? 'md:w-1/2' : 'md:w-3/4'">
+      <div class="md:p-0 p-10"></div>
       <NuxtPage />
+    </div>
+    <div class="transition-all ease-in-out delay-100 duration-300 absolute md:top-1/2 transform md:-translate-y-1/2 md:h-auto w-full" :class="SelectedPage === '/' ? 'md:w-1/2' : 'md:w-1/4'">
+      <div class="w-full md:w-max shadow-2xl md:ml-auto md:translate-x-1/2 backdrop-blur-lg bg-white/40 p-2 rounded-none md:rounded-lg grid md:grid-flow-row md:grid-rows-2 grid-flow-col">
+        <NuxtLink to="/" active-class="bg-black text-white" class="transition-all ease-in-out duration-200 row-span-1 md:py-5 py-1 px-1 m-1 rounded-lg hover:bg-black/20 flex justify-center cursor-pointer">
+          <p class="md:rotate-90 font-mono md:text-sm my-2">About</p>
+        </NuxtLink>
+        <NuxtLink to="/blog" active-class="bg-black text-white" class="transition-all ease-in-out duration-200 row-span-1 md:py-5 py-1 px-1 m-1 rounded-lg hover:bg-black/20 flex justify-center cursor-pointer">
+          <p class="md:rotate-90 font-mono md:text-sm my-2">Blog</p>
+        </NuxtLink>
+      </div>
     </div>
     <div class="md:visible invisible absolute bottom-0 left-0 right-0 bg-black p-3 text-white">
       <div class="grid col-span-3 justify-center">
@@ -15,7 +26,6 @@
 </template>
 
 <script setup lang="ts">
-
 const route = useRoute();
 let SelectedPage = ref(route.path);
 
@@ -94,3 +104,16 @@ useHead({
   ]
 })
 </script>
+
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.25s;
+}
+
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  filter: blur(1rem);
+}
+</style>
---
+++ pages/blog.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="isolate w-full h-full p-10">
+    <h1 class="font-serif font-extrabold text-gray-800 text-7xl">Blog</h1>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
