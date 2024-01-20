aebe5b92ab02db776d71737e87d053b54e58e143
Vinayak Nivalkar <57988280+VinayakNivalkar@users.noreply.github.com>
2024-01-20 15:28:40 +0530

    favicons added

--- app.vue
+++ app.vue
@@ -37,14 +37,59 @@ useSeoMeta({
 })
 
 useHead({
+  link: [
+    {
+      rel: 'apple-touch-icon',
+      sizes: '180x180',
+      href: '/apple-touch-icon.png?v=1'
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '32x32',
+      href: '/favicon-32x32.png?v=1'
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '16x16',
+      href: '/favicon-16x16.png?v=1'
+    },
+    {
+      rel: 'manifest',
+      href: '/site.webmanifest?v=1'
+    },
+    {
+      rel: 'mask-icon',
+      href: '/safari-pinned-tab.svg?v=1',
+      color: '#000000'
+    },
+    {
+      rel: 'shortcut icon',
+      href: '/favicon.ico?v=1'
+    },
+    {
+      rel: 'icon',
+      type: 'image/svg+xml',
+      href: '/favicon.svg?v=1'
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      href: '/favicon.png?v=1'
+    }
+  ],
   htmlAttrs: {
     lang: 'en'
   },
-  link: [
+  meta: [
     {
-      rel: 'icon',
-      type: 'image/svg',
-      href: '/favicon.ico',
+      name: 'msapplication-TileColor',
+      content: '#00aba9'
+    },
+    {
+      name: 'theme-color',
+      content: '#ffffff'
     }
   ]
 })
